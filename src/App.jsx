76e4c65fb42c790fbea787aedb071472fd69d58c
Jude Uzo <judeuzo7@gmail.com>
2026-01-06 17:51:18 +0100

    INVOKED PAGE ANALYTICS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import MainPage from "./pages/MainPage";
+import { Analytics } from "@vercel/analytics/next"
 
 
 export default function App() {
   return (
     <div>
       <MainPage/>
+      <Analytics/>
     </div>
   );
 }
